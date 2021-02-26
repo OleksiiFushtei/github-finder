300626310f5f06f3839419ac16ac08a71f4d42ef
Oleksii <46541097+OleksiiFushtei@users.noreply.github.com>
2021-02-26 11:44:35 +0100

    Hooks and ContextAPI

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+export default function About() {
+	return (
+		<Fragment>
+			<h1>About This App</h1>
+			<p>This app helps you to search users from GitHub</p>
+		</Fragment>
+	)
+}
